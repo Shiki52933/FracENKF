f8bc8c8f25254317e00d2cd6f267078a5ba6a748
Shiki <2575511088@qq.com>
2022-11-14 17:43:56 +0800

    prepare lorenz63

--- lorenz63.cpp
+++ lorenz63.cpp
@@ -29,6 +29,40 @@ mat generate_lorenz63(double sigma, double rho, double beta, double dt, double t
     return sol;
 }
 
+mat H_ob(mat& ensemble){
+    vec a{0, 1, 1};
+    vec b{1, 1, 0};
+    mat H(2, 3);
+    H.row(0) = a;
+    H.row(1) = b;
+    return H * ensemble;
+}
+
+mat model(mat& ensemble, int idx, mat& sys_var){
+    double sigma = 10, rho = 28, beta = 8.0/3;
+    double dt = 0.001;
+    mat sol = ensemble;
+    for(int i=0; i<ensemble.n_cols; i++){
+        double x_old = sol.col(i)[0];
+        double y_old = sol.col(i)[1];
+        double z_old = sol.col(i)[2];
+        double dt_ = dt;
+
+        double x_new = sigma * (y_old - x_old) * dt_ + x_old;
+        double y_new = ( x_old * (rho - z_old) - y_old ) * dt_ + y_old;
+        double z_new = ( x_old * y_old - beta * z_old ) * dt_ + z_old;
+
+        sol.col(i)[0] = x_new;
+        sol.col(i)[1] = y_new;
+        sol.col(i)[2] = z_new;
+    }
+    return sol;
+}
+
+void lorenz63EnKF(){
+
+}
+
 int main(int argc, char** argv){
-    generate_lorenz63(10., 28., 8.0/3, 0.001, 200, vec{0.1,0.1,0.1});
+    generate_lorenz63(10., 28., 8.0/3, 0.001, 200, vec{1,1,1});
 }
