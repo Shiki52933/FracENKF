ce7380fd204171d75c9481b83d5d6a92fd575bf4
Shiki <2575511088@qq.com>
2022-12-26 15:08:18 +0800

    add particle filter for lorenz63

--- ParticleFilter.hpp
+++ ParticleFilter.hpp
@@ -1,15 +1,19 @@
 #pragma once
 #include <armadillo>
 #include <utility>
+#include <exception>
 #include <vector>
+#include "StochasticENKF.hpp"
 
 using arma::vec;
 using arma::mat;
 
+extern std::pair<vec, mat> compute_mean_variance(const mat& ensemble, const vec& weights);
+
 
 template<typename Model, typename Ob>
 std::vector< std::pair<mat, vec> >
-ParticleFilter(mat ensemble, Model model, Ob likehood, std::vector<vec> obResults){
+ParticleFilter(mat ensemble, Model model, Ob likehood, std::vector<vec> obResults, Errors sys_vars){
     int num_iter = obResults.size();
     int ensemble_size = ensemble.n_cols;
     std::vector< std::pair<mat, vec> > results{};
@@ -23,15 +27,35 @@ ParticleFilter(mat ensemble, Model model, Ob likehood, std::vector<vec> obResult
             // 有观测，需要更新权重
             double sum = 0;
             for(int j=0; j<ensemble_size; j++){
-                weights[j] = weights[j] * likehood(ensemble[j], obResults[j]);
+                weights[j] = weights[j] * likehood(ensemble.col(j), obResults[i]);
                 sum += weights[j];
             }
             weights /= sum;
             results.push_back({ensemble, weights});
+
+            auto mean_var = compute_mean_variance(ensemble, weights);
+            ensemble = arma::mvnrnd(mean_var.first, mean_var.second, ensemble_size);
+            weights = vec(ensemble_size, arma::fill::value(1./ensemble_size));
         }
 
-        ensemble = model(ensemble);
+        ensemble = model(ensemble, i, sys_vars[i]);
     }
 
     return results;
 }
+
+std::pair<vec, mat> compute_mean_variance(const mat& ensemble, const vec& weights){
+    vec mean(ensemble.n_rows, arma::fill::zeros);
+    for(int i=0; i<ensemble.n_cols; i++){
+        mean += weights(i) * ensemble.col(i);
+    }
+
+    mat var(ensemble.n_rows, ensemble.n_rows, arma::fill::zeros);
+    for(int j=0; j<ensemble.n_cols; j++){
+        vec devication = ensemble.col(j) - mean;
+        var += weights(j) * devication * devication.t();
+    }
+    
+    return std::pair{mean, var};
+}
+
--- lorenz63.cpp
+++ lorenz63.cpp
@@ -1,4 +1,5 @@
 #include "StochasticENKF.hpp"
+#include "ParticleFilter.hpp"
 #include <armadillo>
 #include <iostream>
 #include <boost/program_options.hpp>
@@ -32,31 +33,31 @@ mat generate_lorenz63(double sigma, double rho, double beta, double dt, double t
         sol.col(i+1) += perturb.col(i);
     } 
     mat sol_t = sol.t();
-    sol_t.save("lorenz63.csv", arma::raw_ascii);
+    sol_t.save("./data/lorenz63.csv", arma::raw_ascii);
     return sol;
 }
 
-mat H_ob(mat& ensemble){
-    vec a{0, 1, 1};
-    vec b{1, 1, 0};
-    mat H(3, 2);
-    H.col(0) = a;
-    H.col(1) = b;
-    H = H.t();
-    return H * ensemble;
-    /*
-    mat ob(2, ensemble.n_cols);
-    for(int i=0; i<ensemble.n_cols; i++){
-        ob(0, i) = sin(ensemble(0, i)) + cos(ensemble(1, i));
-        ob(1, i) = sin(ensemble(1, i)) + cos(ensemble(2, i));
-    }
-    return ob;
-    */
-}
+// mat H_ob(mat& ensemble){
+//     vec a{0, 1, 1};
+//     vec b{1, 1, 0};
+//     mat H(3, 2);
+//     H.col(0) = a;
+//     H.col(1) = b;
+//     H = H.t();
+//     return H * ensemble;
+//     /*
+//     mat ob(2, ensemble.n_cols);
+//     for(int i=0; i<ensemble.n_cols; i++){
+//         ob(0, i) = sin(ensemble(0, i)) + cos(ensemble(1, i));
+//         ob(1, i) = sin(ensemble(1, i)) + cos(ensemble(2, i));
+//     }
+//     return ob;
+//     */
+// }
 
 namespace config{
     double sigma = 10, rho = 28, beta = 8.0/3;
-    double dt = 0.001;
+    double dt = 0.001, max_time=5;
 
     double ob_var = 1;
     double sys_var = 0.01, real_sys_var = 0.;
@@ -103,14 +104,19 @@ void lorenz63EnKF(){
     auto sys_error_ptr = std::make_shared<mat>(3, 3, arma::fill::eye);
     *sys_error_ptr *= sys_var;
     // 参考解
-    mat ref = generate_lorenz63(config::sigma, config::rho, config::beta, config::dt, 200, vec{2,2,2}, *sys_error_ptr*config::real_sys_var);
+    mat ref = generate_lorenz63(config::sigma, config::rho, config::beta, config::dt, config::max_time, vec{2,2,2}, *sys_error_ptr*config::real_sys_var);
+    
+    mat H = arma::randn(2, 3);
+    auto H_ob = [&H](const mat& ensemble){
+        return H * ensemble;
+    };
     mat all_ob = H_ob(ref);
     // 初始值
     vec init_ave{0., 0., 0.};
     mat init_var(3, 3, arma::fill::eye);
     init_var *= init_var_;
     // ob
-    ObserveOperator ob_op = H_ob;
+    auto ob_op = H_ob;
     auto error_ptr = std::make_shared<mat>(2, 2, arma::fill::eye);
     *error_ptr *= ob_var;
 
@@ -158,20 +164,101 @@ void lorenz63EnKF(){
     kurtosis.save("./data/kurtosis.csv", arma::raw_ascii);
 }
 
+void lorenz63particle(){
+    // 参数
+    double ob_var = config::ob_var;
+    double sys_var = config::sys_var;
+    double init_var_ = config::init_var_;
+    int select_every = config::select_every;
+
+    // 系统误差
+    auto sys_error_ptr = std::make_shared<mat>(3, 3, arma::fill::eye);
+    *sys_error_ptr *= sys_var;
+
+    // 参考解
+    mat ref = generate_lorenz63(config::sigma, config::rho, config::beta, config::dt, config::max_time, vec{2,2,2}, *sys_error_ptr*config::real_sys_var);
+    
+    // ob算子
+    mat H = arma::randn(2, 3);
+    auto error_ptr = std::make_shared<mat>(2, 2, arma::fill::eye);
+    *error_ptr *= ob_var;
+
+    // 辅助lambda表达式
+    auto H_ob = [&H](const mat& ensemble){
+        return H * ensemble;
+    };
+    auto likehood = [&H, &error_ptr](vec solid, vec ob){
+        vec ob_ = H * solid;
+        // std::cout<<"enter likehood\n";
+        vec misfit = ob_ - ob;
+        mat likehood = -1./2 * misfit.t() * error_ptr->i() * misfit;
+        // std::cout<<"end likehood\n";
+        return exp(likehood(0, 0));
+    };
+
+    std::vector<vec> ob_list;
+    Errors ob_errors;
+    mat all_ob = H_ob(ref);
+
+    for(int i=0; i<all_ob.n_cols; i++){
+        // std::cout<<"in for\n";
+        ob_errors.add(error_ptr);
+        if(i%select_every == 0)
+            ob_list.push_back(all_ob.col(i)+mvnrnd(vec(2,arma::fill::zeros), *error_ptr));
+        else
+            ob_list.push_back(vec());
+    }
+
+    // 系统误差
+    Errors sys_errors;
+    for(int i=0; i<ob_list.size()+1; i++)
+        sys_errors.add(sys_error_ptr);
+    
+    // 初始值
+    int size = config::ensemble_size;
+    vec init_ave{0., 0., 0.};
+    mat init_var(3, 3, arma::fill::eye);
+    init_var *= init_var_;
+    mat ensemble = mvnrnd(init_ave, init_var, size);
+
+    // particle filter
+    std::cout<<"ready for particle filter\n";
+    auto particle = ParticleFilter(ensemble, model, likehood, ob_list, sys_errors);
+    std::cout<<"particle filter ended\n";
+
+    // 后处理
+    mat assimilated(particle.size(), 3, arma::fill::none);
+
+    for(int i=0; i<particle.size(); i++){
+        vec temp(3, arma::fill::zeros);
+        for(int j=0; j<particle[i].first.n_cols; j++){
+            temp += particle[i].first.col(j) * particle[i].second(j);
+        }
+        assimilated(i, 0) = temp(0);
+        assimilated(i, 1) = temp(1);
+        assimilated(i, 2) = temp(2);
+    }
+    assimilated.save("./data/analysis.csv", arma::raw_ascii);
+}
+
 int main(int argc, char** argv){
     using namespace boost::program_options;
     using namespace config;
 
+    std::string problem;
+
     options_description cmd("lorenz63 EnKF");
+    cmd.add_options()("problem,p", value<std::string>(&problem)->default_value("ENKF"), "type: ENKF or Particle");
     cmd.add_options()("sigma,s", value<double>(&sigma)->default_value(10), "sigma");
     cmd.add_options()("rho,r", value<double>(&rho)->default_value(28), "rho");
     cmd.add_options()("beta,b", value<double>(&beta)->default_value(8.0/3), "beta");
     cmd.add_options()("ob_var,o", value<double>(&ob_var)->default_value(0.1), "ob_error");
-    cmd.add_options()("sys_var,v", value<double>(&sys_var)->default_value(0.01), "system_error");
+    cmd.add_options()("sys_var,v", value<double>(&sys_var)->default_value(0.), "system_error");
     cmd.add_options()("init_var,i", value<double>(&init_var_)->default_value(10), "init_error");
     cmd.add_options()("real_sys_var,rs", value<double>(&real_sys_var)->default_value(0.), "real_system_error");
     cmd.add_options()("select,sl", value<int>(&select_every)->default_value(50), "select every");
     cmd.add_options()("size,n", value<int>(&ensemble_size)->default_value(20), "ensemble size");
+    cmd.add_options()("time,t", value<double>(&max_time)->default_value(5), "max_time");
     
     variables_map map;
     store(parse_command_line(argc, argv, cmd), map);
@@ -188,9 +275,14 @@ int main(int argc, char** argv){
     config::ensemble_size = map["ensemble size"].as<int>();
 */ 
     arma::arma_rng::set_seed_random();
-    lorenz63EnKF();
+    // lorenz63EnKF();
+    if(problem == "ENKF")
+        lorenz63EnKF();
+    else
+        lorenz63particle();
 
-    std::cout<<"sigma: "<<sigma<<'\n'
+    std::cout<<"problem type: "<<problem<<'\n'
+            <<"sigma: "<<sigma<<'\n'
             <<"rho: "<<rho<<'\n'
             <<"beta: "<<beta<<'\n'
             <<"init_var: "<<init_var_<<'\n'
@@ -198,5 +290,6 @@ int main(int argc, char** argv){
             <<"sys_var: "<<sys_var<<'\n'
             <<"real_sys_var: "<<real_sys_var<<'\n'
             <<"select every: "<<select_every<<'\n'
-            <<"ensemble size: "<<ensemble_size<<'\n';
+            <<"ensemble size: "<<ensemble_size<<'\n'
+            <<"max time: "<<max_time<<'\n';
 }
